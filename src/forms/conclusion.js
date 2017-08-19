7798a9c8b172cc5e2c72243fd1db4874df3e481a
Ryan Pfister <ryan.pfister@parivedasolutions.com>
2017-08-19 12:35:18 -0400

    Basic form done

--- src/forms/conclusion.js
+++ src/forms/conclusion.js
@@ -5,6 +5,7 @@ import { FormGroup, ControlLabel, FormControl, HelpBlock, Checkbox, Radio, Butto
 
 const ConclusionForm = (props) => (
   <Form model={props.model} onSubmit={(val) => this.handleSubmit(val)}>
+    <h4>Initial Conclusion</h4>
     <ControlLabel>Style</ControlLabel>
     <Control.select model=".style" className="form-control">
       <option></option>
@@ -18,6 +19,29 @@ const ConclusionForm = (props) => (
       <option value="moderate">Moderate</option>
       <option value="warm">Warm</option>
     </Control.select>
+    <ControlLabel>Age</ControlLabel>
+    <Control.select model=".age" className="form-control">
+      <option></option>
+      <option value="1-3">1-3</option>
+      <option value="3-5">3-5</option>
+      <option value="5-10">5-10</option>
+      <option value="10+">10+</option>
+    </Control.select>
+    <hr />
+    <h4>Final Conclusion</h4>
+    <ControlLabel>Vintage</ControlLabel>
+    <Control.text model=".vintage" className="form-control" />
+    <ControlLabel>Varietals</ControlLabel>
+    <Control.text model=".varietals" className="form-control" />
+    <ControlLabel>Country</ControlLabel>
+    <Control.text model=".country" className="form-control" />
+    <ControlLabel>Region</ControlLabel>
+    <Control.text model=".region" className="form-control" />
+    <ControlLabel>Sub-Region</ControlLabel>
+    <Control.text model=".sub-region" className="form-control" />
+    <ControlLabel>Quality Level</ControlLabel>
+    <Control.text model=".quality-level" className="form-control" />
+    <hr />
   </Form>
 );
 
